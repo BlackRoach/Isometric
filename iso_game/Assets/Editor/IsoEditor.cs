9466fb62bd7de0e60dccfce8f4d368d30145147d
black4roach <black4roach@gmail.com>
2020-07-07 21:23:57 +0900

    add Isometric Tile Editor script
    아이소메트릭 에디터 스크립트 추가
    - Scene 창에 n * n 아이소메트릭 그리드 생성 기능

---
+++ iso_game/Assets/Editor/IsoEditor.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+[CustomEditor(typeof(TileManager))]
+
+
+public class IsoEditor : Editor
+{
+    private TileManager tileManager;
+   
+
+
+    private float tileWidth;
+    private float tileHeight;
+    private const float gridConst = 0.01f;
+    private void OnEnable()
+    {
+        tileManager = (TileManager)target;
+          }
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+    }
+
+    void OnSceneGUI()
+    {
+        SceneView.RepaintAll();
+        tileWidth = tileManager.gridSize * gridConst * .5f;
+        tileHeight = tileManager.gridSize * gridConst * .25f;
+        
+        DrawMapGrid();
+    }
+    private void DrawMapGrid()
+    {
+        //그리드 선 색 설정
+        Handles.color = tileManager.gridColor;
+
+        //왼쪽 아래 방향 그리드 Draw
+        Vector2 startPos = tileManager.transform.position;
+        Vector2 endPos = tileManager.transform.position;
+        
+        startPos.y += tileHeight;
+        endPos.x -= tileManager.mapHeight * tileWidth;
+        endPos.y -= (tileManager.mapHeight - 1) * tileHeight;
+
+        for (int i = 0; i < (tileManager.mapWidth + 1); i++)
+        {
+            Handles.DrawLine(startPos, endPos);
+
+            startPos.x += tileWidth;
+            startPos.y -= tileHeight;
+            endPos.x += tileWidth;
+            endPos.y -= tileHeight;
+        }
+
+        //오른쪽 아래 방향 그리드 Draw
+        startPos = tileManager.transform.position;
+        endPos = tileManager.transform.position;
+
+        startPos.y += tileHeight;
+        endPos.x += tileManager.mapWidth * tileWidth;
+        endPos.y -= (tileManager.mapWidth - 1) * tileHeight;
+
+        for (int i = 0; i < (tileManager.mapHeight + 1); i++)
+        {
+            Handles.DrawLine(startPos, endPos);
+
+            startPos.x -= tileWidth;
+            startPos.y -= tileHeight;
+            endPos.x -= tileWidth;
+            endPos.y -= tileHeight;
+        }
+
+    }
+}
---
+++ iso_game/Assets/Scripts/Manager/TileManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+public class TileManager : MonoBehaviour
+{
+    
+    public int gridSize;
+    public Color gridColor;
+
+    public float mapWidth;
+    public float mapHeight;
+
+
+    
+
+    
+    
+}
